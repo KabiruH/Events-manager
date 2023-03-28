8ae4e83f80570e56d0cc2c28afb75f7800461e47
sainamercy <sainamercy@gmail.com>
2023-03-28 14:35:26 +0300

    client: home page

---
+++ mimik-frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+function Home(){
+    return (
+        <div className="w-full h-screen">
+            <div className= "relative h-full">
+                <img src="https://lh3.googleusercontent.com/gKm8iSrQAkW3Cy02c-0jBPthuoKZJV-WMmjSOPzvGfv2FJaCgzIEhzy4g1OS0JHaVMOmHXEt92jU14Iltq4N5GWKM_J_kLOx6vSyl7MCdQ=h450-rw" alt="hero" className="w-full h-full"/>
+            </div>
+            <div className="absolute h-full bg-white/50 w-full top-0"></div>
+        </div>
+    )
+}
+
+export default Home
