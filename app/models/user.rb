9c693dcd343c23431cd75c25ffa093c6467f72c4
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-03-29 14:37:42 +0300

    finished the controlllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  has_many :attendees
-  has_many :events, through: :attendees
+  has_many :events
+  has_many :attendees, through: :events dependent: :destroy
+    # has password utility func
+    has_secure_password  # hashing of a password 123456 => skdjfksdvj@jnfkdjnsjdf
+    # fields
+    validates :email, presence: true, uniqueness: true
+    validates :age, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, length: {minimum: 6}, if: -> { new_record? || !password_digest.nil?}
+    validates :gender, presence: true
 
-  has_secure_password
+  
 end
