c1f7a1baf18f4695b05944e7aacf5569d9dd6b28
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-03-29 15:47:03 +0300

    final touches

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,5 +1,4 @@
 class SpeakersController < ApplicationController
-    class SpeakersController < ApplicationController
         before_action :set_event
       
         def index
@@ -16,19 +15,19 @@ class SpeakersController < ApplicationController
           if speaker.save
             redirect_to event_speakers_path(event), notice: 'Speaker was successfully created.'
           else
-            render json: 
+            render json: new
           end
         end
       
         private
       
         def set_event
-          event = Event.find(params[:event_id])
+          event = Event.find_by(id:params[:id])
         end
       
         def speaker_params
           params.require(:speaker).permit(:name, :email)
         end
-      end
+      
       
 end
