63488eb0d69679efebb31a005a2b98832ebe7511
sainamercy <sainamercy@gmail.com>
2023-03-30 09:29:33 +0300

    home page updates

--- mimik-frontend/src/components/Home.js
+++ mimik-frontend/src/components/Home.js
@@ -1,15 +1,25 @@
+import Highlights from "./Highlights";
+import EventsHome from "./EventsHome";
 function Home() {
-  return <div className="w-3/4 h-3/4 m-auto flex justify-evenly items-center">
-    <div className="text-gray-700">
-        <h1 className="text-5xl">Welcome to <br/><span className="text-rose-600">MIMIK</span> events</h1>
-    <p>There are two major products that come out of Berkeley: LSD and UNIX. We don’t believe this to be a coincidence. (Jeremy S. Anderson)</p>
+  return <div>
+   <div className="h-screen">
+     <div className="relative h-[98%] flex items-center w-[99%] justify-end pr-40 bg-[url(https://img.freepik.com/free-photo/blue-designed-grunge-concrete-texture-vintage-background-with-space-text-image_1258-108928.jpg)] bg-cover">
+     <video autoPlay muted loop className="h-5/6">
+          <source
+            src="https://www.zohowebstatic.com/sites/zweb/images/backstage/home/banner-intro.webm"
+            type="video/mp4"
+          />
+        </video>
     </div>
-    <div className="flex gap-6">
-    <img src="mimik-frontend/src/assets/mimik-logo.png" alt="logo"/>
-    <img src="mimik-frontend/src/assets/mimik-logo.png" alt="logo"/>
-    <img src="mimik-frontend/src/assets/mimik-logo.png" alt="logo"/>
+    <div className="text-white text-xl flex flex-col pl-52 justify-center gap-8 absolute top-20 bg-white/30 h-full w-full">
+        <h1 className="text-6xl font-semibold uppercase">Welcome to <br/><span className="text-rose-600">MIMIK</span> events.</h1>
+    <p>There are two major products that come out of Berkeley: LSD and UNIX.<br/> We don’t believe this to be a coincidence. (Jeremy S. Anderson)</p>
+    <button className="bg-rose-600 rounded-lg w-48 p-2 text-white hover:opacity-80">Get started <i className="fa-solid fa-arrow-right ml-1"></i></button>
     </div>
-  </div>;
+     </div>
+     <Highlights/>
+     <EventsHome/>
+     </div>
 }
 
 export default Home;
--- mimik-frontend/src/components/Navbar.js
+++ mimik-frontend/src/components/Navbar.js
@@ -1,22 +1,23 @@
+import React from 'react';
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between bg-white h-20 text-gray-600 w-1/2 mx-auto">
+    <nav className="flex items-center justify-between bg-white h-20 text-gray-600 w-full px-60">
       <div>
-        <img src="mimik-frontend/assets/mimik-logo.png" alt="logo"/>
+        <img src="/assets/logonobg.png" alt="logo" className="h-40"/>
       </div>
       <div>
-        <ul className="flex gap-6">
+        <ul className="flex gap-6 items-center">
           <li>
-            Home
+          <i className="fa-sharp fa-solid fa-house mr-1"></i>Home
           </li>
           {/* <li>
             Events
           </li> */}
           <li>
-            Login
+          <i className="fa-solid fa-right-to-bracket mr-1"></i>Login
           </li>
-          <li>
-            Signup
+          <li className="bg-rose-600 p-2 text-white rounded-lg hover:opacity-80">
+          Signup<i className="fa-solid fa-arrow-right ml-1"></i>
           </li>
         </ul>
       </div>
