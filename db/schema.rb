66adf85e5fba5be3affa7e585634e8be5c0eb9d9
Iris <iris.thuo@student.moringaschool.com>
2023-04-02 13:43:53 +0300

    Relationship btwn events and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_064349) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_104125) do
   create_table "attendees", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_02_064349) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "event_id"
   end
 
 end
