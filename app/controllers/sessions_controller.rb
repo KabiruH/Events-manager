885626b2866aa1f4bdabe7eeacdb33d72b8b4553
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-04-02 17:21:27 +0300

    back to bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,41 +1,20 @@
 class SessionsController < ApplicationController
-    before_action :authorize?, only: :destroy
-
     def create
-      user = User.find_by(email: params[:email])
-      if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: { message: "Welcome, #{user.username}!" }
-      else
-        render json: { error: "Invalid username or password" }, status: :unauthorized
-      end
-    end
+        user = User.find_by(username: params[:username])
 
-
-    def destroy
-        return render json: {errors: ["User not found", "kindly log in"]}, status: :unauthorized unless session.include? :user_id
-        session.delete :user_id
-        render json: { message: "Goodbye!"}
-    end
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: ["Invalid username or password"]}, status: :unauthorized
+        end
   end
 
-
-    #     def create
-#         user = User.find_by(username: params[:username])
-
-#         if user&.authenticate(params[:password])
-#             session[:user_id] = user.id
-#             render json: user, status: :created
-#         else
-#             render json: {errors: ["Invalid username or password"]}, status: :unauthorized
-#         end
-#   end
-
-#   def destroy
-#  return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
-#     session.delete :user_id
-#     head :no_content
-#       #return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
-#   end
-# end
+  def destroy
+ return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+    session.delete :user_id
+    head :no_content
+      #return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+  end
+end
 
